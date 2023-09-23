f4ad7c03b19cb510c1eb9489a0523b147c84c199
Shiigu <cristi1991_10@hotmail.com>
2023-09-23 18:04:02 -0300

    Made Step Parameters Form more informative

--- RogueCustomsDungeonEditor/HelperForms/frmActionParameters.Designer.cs
+++ RogueCustomsDungeonEditor/HelperForms/frmActionParameters.Designer.cs
@@ -30,18 +30,20 @@
         {
             lblDisplayName = new Label();
             tlpParameters = new TableLayoutPanel();
-            llblWiki = new LinkLabel();
+            llblWikiAction = new LinkLabel();
             btnSave = new Button();
             btnCancel = new Button();
             lblRequired = new Label();
+            lblDescription = new Label();
+            llblWikiParameters = new LinkLabel();
             SuspendLayout();
             // 
             // lblDisplayName
             // 
-            lblDisplayName.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            lblDisplayName.Font = new Font("Segoe UI", 13F, FontStyle.Bold, GraphicsUnit.Point);
             lblDisplayName.Location = new Point(11, 7);
             lblDisplayName.Name = "lblDisplayName";
-            lblDisplayName.Size = new Size(296, 63);
+            lblDisplayName.Size = new Size(314, 32);
             lblDisplayName.TabIndex = 0;
             lblDisplayName.Text = "DisplayName:";
             // 
@@ -51,7 +53,7 @@
             tlpParameters.ColumnCount = 2;
             tlpParameters.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
             tlpParameters.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 70F));
-            tlpParameters.Location = new Point(11, 112);
+            tlpParameters.Location = new Point(11, 115);
             tlpParameters.Name = "tlpParameters";
             tlpParameters.RowCount = 1;
             tlpParameters.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
@@ -59,20 +61,20 @@
             tlpParameters.TabIndex = 1;
             tlpParameters.SizeChanged += tlpParameters_SizeChanged;
             // 
-            // llblWiki
+            // llblWikiAction
             // 
-            llblWiki.AutoSize = true;
-            llblWiki.Location = new Point(12, 414);
-            llblWiki.Name = "llblWiki";
-            llblWiki.Size = new Size(286, 15);
-            llblWiki.TabIndex = 3;
-            llblWiki.TabStop = true;
-            llblWiki.Text = "Click here to get more information about parameters";
-            llblWiki.LinkClicked += llblWiki_LinkClicked;
+            llblWikiAction.AutoSize = true;
+            llblWikiAction.Location = new Point(10, 441);
+            llblWikiAction.Name = "llblWikiAction";
+            llblWikiAction.Size = new Size(282, 15);
+            llblWikiAction.TabIndex = 3;
+            llblWikiAction.TabStop = true;
+            llblWikiAction.Text = "Click here to get more information about this action";
+            llblWikiAction.LinkClicked += llblWikiAction_LinkClicked;
             // 
             // btnSave
             // 
-            btnSave.Location = new Point(69, 441);
+            btnSave.Location = new Point(68, 466);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(75, 23);
             btnSave.TabIndex = 4;
@@ -82,7 +84,7 @@
             // 
             // btnCancel
             // 
-            btnCancel.Location = new Point(184, 441);
+            btnCancel.Location = new Point(183, 466);
             btnCancel.Name = "btnCancel";
             btnCancel.Size = new Size(75, 23);
             btnCancel.TabIndex = 5;
@@ -92,21 +94,42 @@
             // 
             // lblRequired
             // 
-            lblRequired.Location = new Point(10, 76);
+            lblRequired.Location = new Point(10, 79);
             lblRequired.Name = "lblRequired";
             lblRequired.Size = new Size(315, 33);
             lblRequired.TabIndex = 6;
             lblRequired.Text = "Fields with an * require a value.\r\nFields with an ^ require a value on at least one of them.";
             // 
+            // lblDescription
+            // 
+            lblDescription.Location = new Point(10, 43);
+            lblDescription.Name = "lblDescription";
+            lblDescription.Size = new Size(315, 33);
+            lblDescription.TabIndex = 7;
+            lblDescription.Text = "Description of Action";
+            // 
+            // llblWikiParameters
+            // 
+            llblWikiParameters.AutoSize = true;
+            llblWikiParameters.Location = new Point(10, 416);
+            llblWikiParameters.Name = "llblWikiParameters";
+            llblWikiParameters.Size = new Size(286, 15);
+            llblWikiParameters.TabIndex = 8;
+            llblWikiParameters.TabStop = true;
+            llblWikiParameters.Text = "Click here to get more information about parameters";
+            llblWikiParameters.LinkClicked += llblWikiParameters_LinkClicked;
+            // 
             // frmActionParameters
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(337, 474);
+            ClientSize = new Size(337, 495);
+            Controls.Add(llblWikiParameters);
+            Controls.Add(lblDescription);
             Controls.Add(lblRequired);
             Controls.Add(btnCancel);
             Controls.Add(btnSave);
-            Controls.Add(llblWiki);
+            Controls.Add(llblWikiAction);
             Controls.Add(tlpParameters);
             Controls.Add(lblDisplayName);
             FormBorderStyle = FormBorderStyle.FixedDialog;
@@ -122,9 +145,11 @@
 
         private Label lblDisplayName;
         private TableLayoutPanel tlpParameters;
-        private LinkLabel llblWiki;
+        private LinkLabel llblWikiAction;
         private Button btnSave;
         private Button btnCancel;
         private Label lblRequired;
+        private Label lblDescription;
+        private LinkLabel llblWikiParameters;
     }
 }
