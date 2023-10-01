b50768420c663fb9caa2cc9633f0ca16b9dbf944
Shiigu <cristi1991_10@hotmail.com>
2023-10-01 15:18:59 -0300

    Updated ReplaceConsoleRepresentation to give BackColor parameter + Converting older JSONs to support it

---
+++ RogueCustomsDungeonEditor/Utils/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueCustomsDungeonEditor.Utils
+{
+    public static class Constants
+    {
+        public const string CurrentDungeonVersion = "1.1";
+    }
+}
--- RogueCustomsDungeonEditor/frmMain.cs
+++ RogueCustomsDungeonEditor/frmMain.cs
@@ -2,6 +2,7 @@ using RogueCustomsDungeonEditor.EffectInfos;
 using RogueCustomsDungeonEditor.FloorInfos;
 using RogueCustomsDungeonEditor.HelperForms;
 using RogueCustomsDungeonEditor.Utils;
+using RogueCustomsDungeonEditor.Utils.DungeonInfoConversion;
 using RogueCustomsDungeonEditor.Validators;
 using RogueCustomsGameEngine.Utils.Helpers;
 using RogueCustomsGameEngine.Utils.JsonImports;
@@ -18,8 +19,6 @@ namespace RogueCustomsDungeonEditor
 {
     public partial class frmMain : Form
     {
-        private const string CurrentDungeonVersion = "1.0";
-
         private readonly Dictionary<TabTypes, TabPage> TabsForNodeTypes = new();
         private readonly List<string> MandatoryLocaleKeys = new();
         private readonly List<string> BaseLocaleLanguages = new();
@@ -397,7 +396,7 @@ namespace RogueCustomsDungeonEditor
                 }
             }
             ActiveDungeon = DungeonInfoHelpers.CreateEmptyDungeonTemplate(LocaleTemplate, BaseLocaleLanguages);
-            ActiveDungeon.Version = CurrentDungeonVersion;
+            ActiveDungeon.Version = Constants.CurrentDungeonVersion;
             RefreshTreeNodes();
             tvDungeonInfo.SelectedNode = tvDungeonInfo.TopNode;
             tvDungeonInfo.Focus();
@@ -445,8 +444,8 @@ namespace RogueCustomsDungeonEditor
                     {
                         PropertyNameCaseInsensitive = true
                     });
-                    if (string.IsNullOrWhiteSpace(ActiveDungeon.Version))
-                        ActiveDungeon.Version = CurrentDungeonVersion;
+                    var formerVersion = !string.IsNullOrWhiteSpace(ActiveDungeon.Version) ? new string(ActiveDungeon.Version) : "1.0";
+                    ActiveDungeon.ConvertDungeonInfoIfNeeded();
                     tbTabs.TabPages.Clear();
                     DirtyEntry = false;
                     DirtyTab = false;
@@ -460,7 +459,13 @@ namespace RogueCustomsDungeonEditor
                     DungeonPath = ofdDungeon.FileName;
                     sfdDungeon.InitialDirectory = Path.GetDirectoryName(ofdDungeon.FileName);
                     this.Text = $"Rogue Customs Dungeon Editor - [{Path.GetFileName(DungeonPath)}]";
-                    DirtyDungeon = false;
+                    if (!ActiveDungeon.Version.Equals(formerVersion))
+                    {
+                        MessageBox.Show($"Dungeon has been found to belong to outdated version {formerVersion}.\n\nDungeon has been updated to {ActiveDungeon.Version} with default values. Please check differences to ensure they work as intended, or adjust them if needed.\n\nRemember to save the Dungeon afterwards.", "Open Dungeon", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        DirtyDungeon = true;
+                    }
+                    else
+                        DirtyDungeon = false;
                 }
                 catch (Exception ex)
                 {
