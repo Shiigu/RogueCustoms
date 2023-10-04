6f1ba06f5700a3e0f63c512c0ad0eca9ab80c4da
Shiigu <cristi1991_10@hotmail.com>
2023-10-04 18:32:00 -0300

    Refactored Console Representation selections into a UserControl

---
+++ RogueCustomsDungeonEditor/Controls/ConsoleRepresentationSelector.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace RogueCustomsDungeonEditor.Controls
+{
+    partial class ConsoleRepresentationSelector
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnChangeConsoleCharacterBackColor = new Button();
+            btnChangeConsoleCharacterForeColor = new Button();
+            btnChangeConsoleCharacter = new Button();
+            lblConsoleRepresentation = new Label();
+            SuspendLayout();
+            // 
+            // btnChangeConsoleCharacterBackColor
+            // 
+            btnChangeConsoleCharacterBackColor.Location = new Point(73, 58);
+            btnChangeConsoleCharacterBackColor.Name = "btnChangeConsoleCharacterBackColor";
+            btnChangeConsoleCharacterBackColor.Size = new Size(135, 23);
+            btnChangeConsoleCharacterBackColor.TabIndex = 115;
+            btnChangeConsoleCharacterBackColor.Text = "Change Background...";
+            btnChangeConsoleCharacterBackColor.UseVisualStyleBackColor = true;
+            btnChangeConsoleCharacterBackColor.Click += btnChangeConsoleCharacterBackColor_Click;
+            // 
+            // btnChangeConsoleCharacterForeColor
+            // 
+            btnChangeConsoleCharacterForeColor.Location = new Point(73, 29);
+            btnChangeConsoleCharacterForeColor.Name = "btnChangeConsoleCharacterForeColor";
+            btnChangeConsoleCharacterForeColor.Size = new Size(135, 23);
+            btnChangeConsoleCharacterForeColor.TabIndex = 114;
+            btnChangeConsoleCharacterForeColor.Text = "Change Foreground...";
+            btnChangeConsoleCharacterForeColor.UseVisualStyleBackColor = true;
+            btnChangeConsoleCharacterForeColor.Click += btnChangeConsoleCharacterForeColor_Click;
+            // 
+            // btnChangeConsoleCharacter
+            // 
+            btnChangeConsoleCharacter.Location = new Point(73, 0);
+            btnChangeConsoleCharacter.Name = "btnChangeConsoleCharacter";
+            btnChangeConsoleCharacter.Size = new Size(135, 23);
+            btnChangeConsoleCharacter.TabIndex = 113;
+            btnChangeConsoleCharacter.Text = "Change Character...";
+            btnChangeConsoleCharacter.UseVisualStyleBackColor = true;
+            btnChangeConsoleCharacter.Click += btnChangeConsoleCharacter_Click;
+            // 
+            // lblConsoleRepresentation
+            // 
+            lblConsoleRepresentation.Font = new Font("Courier New", 36F, FontStyle.Bold, GraphicsUnit.Point);
+            lblConsoleRepresentation.Location = new Point(0, 9);
+            lblConsoleRepresentation.Name = "lblConsoleRepresentation";
+            lblConsoleRepresentation.Size = new Size(64, 64);
+            lblConsoleRepresentation.TabIndex = 112;
+            lblConsoleRepresentation.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // ConsoleRepresentationSelector
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnChangeConsoleCharacterBackColor);
+            Controls.Add(btnChangeConsoleCharacterForeColor);
+            Controls.Add(btnChangeConsoleCharacter);
+            Controls.Add(lblConsoleRepresentation);
+            Name = "ConsoleRepresentationSelector";
+            Size = new Size(211, 83);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnChangeConsoleCharacterBackColor;
+        private Button btnChangeConsoleCharacterForeColor;
+        private Button btnChangeConsoleCharacter;
+        private Label lblConsoleRepresentation;
+    }
+}
---
+++ RogueCustomsDungeonEditor/Controls/ConsoleRepresentationSelector.cs
@@ -0,0 +1,122 @@
+﻿using RogueCustomsDungeonEditor.HelperForms;
+using RogueCustomsDungeonEditor.Utils;
+using RogueCustomsGameEngine.Utils.Representation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RogueCustomsDungeonEditor.Controls
+{
+    public partial class ConsoleRepresentationSelector : UserControl
+    {
+        private ConsoleRepresentation consoleRepresentation = new();
+
+        public ConsoleRepresentation ConsoleRepresentation => new()
+        {
+            Character = consoleRepresentation.Character,
+            ForegroundColor = consoleRepresentation.ForegroundColor,
+            BackgroundColor = consoleRepresentation.BackgroundColor
+        };
+
+        public event EventHandler<PropertyChangedEventArgs> PropertyChanged;
+
+        public char Character
+        {
+            get { return consoleRepresentation.Character; }
+            set
+            {
+                if (consoleRepresentation.Character != value)
+                {
+                    consoleRepresentation.Character = value;
+                    OnPropertyChanged(nameof(Character));
+                }
+            }
+        }
+
+        public GameColor ForegroundColor
+        {
+            get { return consoleRepresentation.ForegroundColor; }
+            set
+            {
+                if (consoleRepresentation.ForegroundColor != value)
+                {
+                    consoleRepresentation.ForegroundColor = value;
+                    OnPropertyChanged(nameof(ForegroundColor));
+                }
+            }
+        }
+
+        public GameColor BackgroundColor
+        {
+            get { return consoleRepresentation.BackgroundColor; }
+            set
+            {
+                if (consoleRepresentation.BackgroundColor != value)
+                {
+                    consoleRepresentation.BackgroundColor = value;
+                    OnPropertyChanged(nameof(BackgroundColor));
+                }
+            }
+        }
+
+        private void UpdateLabel()
+        {
+            lblConsoleRepresentation.Text = consoleRepresentation.Character.ToString();
+            lblConsoleRepresentation.ForeColor = consoleRepresentation.ForegroundColor.ToColor();
+            lblConsoleRepresentation.BackColor = consoleRepresentation.BackgroundColor.ToColor();
+        }
+
+        public ConsoleRepresentationSelector()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeConsoleCharacter_Click(object sender, EventArgs e)
+        {
+            var characterMapForm = new CharacterMapInputBox(CharHelpers.GetIBM437PrintableCharacters(), (!string.IsNullOrWhiteSpace(lblConsoleRepresentation.Text)) ? lblConsoleRepresentation.Text[0] : '\0');
+            characterMapForm.ShowDialog();
+            if (characterMapForm.Saved)
+            {
+                Character = (char) characterMapForm.CharacterToSave;
+            }
+        }
+
+        private void btnChangeConsoleCharacterForeColor_Click(object sender, EventArgs e)
+        {
+            var colorDialog = new ColorDialog
+            {
+                Color = lblConsoleRepresentation.ForeColor
+            };
+            colorDialog.CustomColors = new int[] { ColorTranslator.ToOle(colorDialog.Color) };
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                ForegroundColor = new GameColor(colorDialog.Color);
+            }
+        }
+
+        private void btnChangeConsoleCharacterBackColor_Click(object sender, EventArgs e)
+        {
+            var colorDialog = new ColorDialog
+            {
+                Color = lblConsoleRepresentation.BackColor
+            };
+            colorDialog.CustomColors = new int[] { ColorTranslator.ToOle(colorDialog.Color) };
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                BackgroundColor = new GameColor(colorDialog.Color);
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            UpdateLabel();
+        }
+    }
+}
