cd9d3d4289dc66aadb96184aad5bc6453fd6aa5b
Shiigu <cristi1991_10@hotmail.com>
2023-10-14 00:41:26 -0300

    Version 1.6.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
 [assembly: AssemblyFileVersion("1.1")]
-[assembly: AssemblyInformationalVersion("1.5.0-beta")]
+[assembly: AssemblyInformationalVersion("1.6.0-beta")]
